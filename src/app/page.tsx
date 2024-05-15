c8bf7ad611c8cd694e906dc13d342944550424a4
ayatat <ayatat@nxt-sys.jp>
2024-05-15 17:53:39 +0900

    :sparkles: [main] ページ遷移機能を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <h1>
+      {/* Next.js のアプリの外にある外部のページにリンクを貼る必要があるときは、Link無しで<a>タグだけを使う */}
+      {/* classNameを使う場合も、Link無しで<a>タグだけを使う */}
       Read <Link legacyBehavior href="/posts/first-post"><a>this page!</a></Link>
     </h1>
   );
