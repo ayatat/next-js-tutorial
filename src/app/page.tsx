b5cbde3b66e7cbab8313db09ff5e5bc6d1009d51
ayatat <ayatat@nxt-sys.jp>
2024-05-16 18:15:48 +0900

    :+1: [mian] プロフィールを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import Link from 'next/link'
+import Head from 'next/head'
+import Layout, { siteTitle } from './components/layout'
+import utilStyles from './styles/utils.module.css'
 
 export default function Home() {
   return (
-    <h1>
-      {/* Next.js のアプリの外にある外部のページにリンクを貼る必要があるときは、Link無しで<a>タグだけを使う */}
-      {/* classNameを使う場合も、Link無しで<a>タグだけを使う */}
-      Read <Link legacyBehavior href="/posts/first-post"><a>this page!</a></Link>
-    </h1>
-  );
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
 }
