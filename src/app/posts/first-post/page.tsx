bd7f2c64c0ea8b021f7a2590eb0782a11dd878f4
ayatat <ayatat@nxt-sys.jp>
2024-05-16 14:10:13 +0900

    :+1: [main] Head要素を追加

--- src/app/posts/first-post/page.tsx
+++ src/app/posts/first-post/page.tsx
@@ -2,7 +2,9 @@ import Link from 'next/link'
 
 export default function FirstPost() {
   return (
-    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link legacyBehavior href="/">
